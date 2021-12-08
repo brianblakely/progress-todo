0cbab747a19297e5068c2c4a3f8ae44eaf72fbf9
Brian Blakely <anewpage.media@gmail.com>
2021-12-08 18:31:49 -0500

    Properly activate view.

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,150 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
+class ColorsViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "progress-todo";
+
+  private _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    webviewView.webview.onDidReceiveMessage((data) => {
+      switch (data.type) {
+        case "colorSelected": {
+          vscode.window.activeTextEditor?.insertSnippet(
+            new vscode.SnippetString(`#${data.value}`)
+          );
+          break;
+        }
+      }
+    });
+  }
+
+  public addColor() {
+    if (this._view) {
+      this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+      this._view.webview.postMessage({ type: "addColor" });
+    }
+  }
+
+  public clearColors() {
+    if (this._view) {
+      this._view.webview.postMessage({ type: "clearColors" });
+    }
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+    );
+
+    // Do the same for the stylesheet.
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
+
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+
+				<title>Cat Colors</title>
+			</head>
+			<body>
+				<ul class="color-list">
+				</ul>
+				<button class="add-color-button">Add Color</button>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+  }
+}
+
+function getNonce() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
+  // const provider = new ColorsViewProvider(context.extensionUri);
+
+  // context.subscriptions.push(
+  //   vscode.window.registerWebviewViewProvider(
+  //     ColorsViewProvider.viewType,
+  //     provider
+  //   )
+  // );
+
+  // context.subscriptions.push(
+  //   vscode.commands.registerCommand("calicoColors.addColor", () => {
+  //     provider.addColor();
+  //   })
+  // );
+
+  // context.subscriptions.push(
+  //   vscode.commands.registerCommand("calicoColors.clearColors", () => {
+  //     provider.clearColors();
+  //   })
+  // );
+  // // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "progress-todo" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "progress-todo.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World 2 from Progress!");
-    }
-  );
+  // // The command has been defined in the package.json file
+  // // Now provide the implementation of the command with registerCommand
+  // // The commandId parameter must match the command field in package.json
+  // let disposable = vscode.commands.registerCommand(
+  //   "progress-todo.helloWorld",
+  //   () => {
+  //     // The code you place here will be executed every time your command is executed
+  //     // Display a message box to the user
+  //     vscode.window.showInformationMessage("Hello World 2 from Progress!");
+  //   }
+  // );
 
-  context.subscriptions.push(disposable);
+  // context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
